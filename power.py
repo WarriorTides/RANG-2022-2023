88f15f53c524605db62172d895785c7df1c62ea2
Silverleaf Yen <silverleaf.yen@gmail.com>
2023-02-27 16:45:14 -0800

    2/27

--- power.py
+++ power.py
@@ -1,44 +1,60 @@
 import pandas as pd
 import numpy as np
+import array
 # def closest(lst, K):
 #     return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
 
 def closest(lst, K):
-     
      lst = np.asarray(lst)
      idx = (np.abs(lst - K)).argmin()
-     return lst[idx]
+    #  return lst[idx]
+     return idx
 
-
-x = 20 # amp limit
-t = 4 # total number of motors; 2 - 4
-b = 1776 # range of power, up to 1899
-r = 1 # 0 - 1; reads off of joystick, magnitude of vector
-
+def motors(arr):
+    cnt = 0
+    for i in arr:
+        if arr[i] != 0:
+            cnt += 1
+    return cnt
 # formula: (x / t) * r
+def power(arr):
+    x = 20 # amp limit
+    t = motors(arr) # total number of motors running
 
-df = pd.read_excel(r"C:\Users\ISS\Downloads\datasheetT200Thruster.xlsx")
+    df = pd.read_excel(r"C:\\Users\\ISS\Downloads\datasheetT200Thruster.xlsx")
+    # get data
+    pwn = df.iloc[:, 0].tolist() # pwn colunmn
+    current = df.iloc[:, 2].tolist() # current colunmn
 
-# get data
-pwn = df.iloc[:, 0].tolist()
-current = df.iloc[:, 2].tolist()
+    sum = 0
+    for i in arr:
+        sum += abs(arr[i])
 
-try:
-    idx = pwn.index(b)
-    ampCur = current[idx] 
-except ValueError:
-    # get values on either side of current 
-    c1 =  closest(pwn, b)
-    idx = pwn.index(c1)
-    
-    ampCur = current[idx] + ((current[idx + 1] - current[idx]) * (b - c1) / 4)
-    # print ("ampCur ", ampCur)
+    out = []
+    for i in range(len(arr)):
+        
+        r = abs(arr[i])
+        amp = r / sum
+        if r == 0:
+            out.append(1500)
+            continue
+        try:
+            idx = current.index(amp)
+            pwnCur = current[idx]
+            if arr[i] < 0:
+                out.append(pwnCur * (-1)) 
+            else:
+                out.append(pwnCur)
+        except ValueError as e:
+            # get idx of value closest
+            idx =  closest(current, amp)
+            pwnCur = pwn[idx]
+            if arr[i] < 0:
+                out.append(pwnCur * (-1)) 
+            else:
+                out.append(pwnCur)
 
-if ampCur > 20:
-    ampCur = 20
+    return out
 
-# apply formula
-ans = (ampCur / t ) * r
-print (ans)
-
+print(power([1,1,1,1,0,0]))
 
